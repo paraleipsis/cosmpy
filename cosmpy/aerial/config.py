b36254506a56d24f89bf9641e202bac276fec859
paraleipsis <83767469+paraleipsis@users.noreply.github.com>
2024-04-01 15:39:44 +0800

    Update config.py

--- cosmpy/aerial/config.py
+++ cosmpy/aerial/config.py
@@ -52,7 +52,7 @@ URL_PREFIXES = (
 @dataclass
 class NetworkType:
     curve: Curve = field(default_factory=ecdsa.SECP256k1)
-    account: Union[BaseAccount, EthAccount, InjAccount] = field(default=BaseAccount())
+    account: Union[BaseAccount, EthAccount, InjAccount] = field(default_factory=BaseAccount())
     hash_function: Callable = hashlib.sha256
     public_key_generator: Callable = _create_proto_public_key
 
